89666209c5aeee4d13187192ba1779b76502f330
Enrique720 <enrique.sobrados@utec.edu.pe>
2020-06-11 00:10:27 -0500

    all maping

--- src/main/java/LearningPeru/ing_software/test/controller/AuthController.java
+++ src/main/java/LearningPeru/ing_software/test/controller/AuthController.java
@@ -3,20 +3,21 @@ package LearningPeru.ing_software.test.controller;
 import LearningPeru.ing_software.test.Entity.User;
 import LearningPeru.ing_software.test.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @Controller
 @RequestMapping(value = "/Auth")
 public class AuthController {
 
     @Autowired
     UserService userService;
+
+    @GetMapping(value = "/all")
+    List<User> find_all(){return userService.getAll();}
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
@@ -29,6 +30,5 @@ public class AuthController {
     List<User> FindAllByUsername(@RequestBody User user){
         return userService.getbyusername(user);
     }
-
 
 }
