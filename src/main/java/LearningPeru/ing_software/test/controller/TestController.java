e9aae6e1aab0152ee755e862dd6a2e53946e9b19
cesar214567 <cesar.madera@utec.edu.pe>
2020-06-19 00:03:27 -0500

    fixes

--- src/main/java/LearningPeru/ing_software/test/controller/TestController.java
+++ src/main/java/LearningPeru/ing_software/test/controller/TestController.java
@@ -18,7 +18,7 @@ public class TestController {
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
-    void tester(){
+    List<User> tester(){
         User user= new User();
         user.setUsername("jbellido");
         user.setPassword(DigestUtils.sha256Hex("jbellido"));
@@ -34,25 +34,36 @@ public class TestController {
         user2.setPassword(DigestUtils.sha256Hex("yserrano"));
         user2.setName("Yamilet");
         user2.setLastname("Serrano");
-        user.setType(Type_of_user.CURATOR);
+        user2.setType(Type_of_user.CURATOR);
         userService.create(user2);
 
         User user3=new User();
         user3.setUsername("josemaria");
         user3.setPassword(DigestUtils.sha256Hex("josemaria"));
-        user3.setName("jose");
-        user3.setLastname("maria");
-        user.setType(Type_of_user.TEACHER);
+        user3.setName("Jose");
+        user3.setLastname("Maria");
+        user3.setType(Type_of_user.TEACHER);
         userService.create(user3);
 
         User user4=new User();
         user4.setUsername("silvialabandera");
         user4.setPassword(DigestUtils.sha256Hex("silvialabandera"));
-        user4.setName("maria");
+        user4.setName("Silvia");
         user4.setLastname("La bandera");
-        user.setType(Type_of_user.STUDENT);
+        user4.setType(Type_of_user.STUDENT);
         userService.create(user4);
 
+        User user5=new User();
+        user5.setUsername("enrique");
+        user5.setPassword(DigestUtils.sha256Hex("enrique"));
+        user5.setName("Enrique");
+        user5.setLastname("Sobrados");
+        user5.setType(Type_of_user.TWAITING);
+        userService.create(user5);
+
+
+
+        return userService.getAll();
 
     }
 }
