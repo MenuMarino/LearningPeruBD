321ea066b97f37a728ec40eab1ca65cb056642f6
Enrique720 <enrique.sobrados@utec.edu.pe>
2020-06-24 12:49:11 -0500

    Added attributes to user

--- src/main/java/LearningPeru/ing_software/test/Entity/User.java
+++ src/main/java/LearningPeru/ing_software/test/Entity/User.java
@@ -22,9 +22,13 @@ public class User {
     @Column
     private String email;       //
 
+    @Column
+    private String Institucion;
 
+    @Column
+    private String Especialidad;
 
-        @Column(unique = true)
+    @Column(unique = true)
     private String username;      //
 
     @Column
@@ -68,6 +72,18 @@ public class User {
     public void setType(String type) {
         this.type = type;
     }*/
+
+    public String getInstitucion() { return Institucion;  }
+
+    public void setInstitucion(String institucion) {  Institucion = institucion;  }
+
+    public String getEspecialidad() {
+        return Especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        Especialidad = especialidad;
+    }
 
     public Type_of_user getType() {
         return type;
